ab5ff868b196cf627e30b51f7b6b9378bf9b3a1b
dushyanineha <dushyanineha@hotmail.com>
2021-04-05 13:49:50 +0530

    Removed unused imports.

--- game/src/main/java/com/neha/lab/SnakesAndLaddersBoard.java
+++ game/src/main/java/com/neha/lab/SnakesAndLaddersBoard.java
@@ -19,7 +19,6 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 // this class is for designing the snakes and ladders board
